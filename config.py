d39c145436bc0891c139f6552f34acb77206678c
Niperx <kharon98@gmail.com>
2025-02-28 00:07:18 +1100

    added info

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
     'sqlite:///' + os.path.join(basedir, 'db/main.db')
-    kp_API_KEY = '<your-token>' # Кинопоиск
-    openAI_API_KEY = '<your-token>'
+    kp_API_KEY = '89833804-3dc2-4fac-9d08-a0603f43c874' # Кинопоиск
+    openAI_API_KEY = "sk-MsYz8DlX7XHgtxT4fifxj3RVfvUWOb1D"
